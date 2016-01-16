d890522b2a8a77808f60ceed9d153d24ef35cee7
Jan Sanchez <joejansanchez@gmail.com>
2016-01-16 02:51:50 -0500

    wip(post): agrego imagen

--- gulpfile.js
+++ gulpfile.js
@@ -6,55 +6,17 @@
  */
 
 var gulp = require('gulp'),
-	mi = require('gulp-complexity'),
-	jshint = require('gulp-jshint'),
-	sequential = require('run-sequence');
-
-var lint = {
-	jshintrc: '.jshintrc',
-	reporterStyle: 'jshint-stylish',
-	reporter: 'fail'
-};
-
-var path = 'source/**/**/*.js';
-
-var jsTasks = ['js'];
+	mi = require('gulp-complexity');
 
 
 /*!!
  *
- * Tarea para lintear el código Javascript
+ * Tarea para evaluar la mantenibilidad
+ * del código fuente 
+ *
  */
-
-gulp.task('lint', function() {
-	return gulp.src(path)
-	.pipe(jshint(lint.jshintrc))
-	.pipe(jshint.reporter(lint.reporterStyle))
-	.pipe(jshint.reporter(lint.reporter));
-});
 
 gulp.task('mi', function(){
-	return gulp.src(path)
+	return gulp.src('jquery-for-test/**/**/*.js')
 	.pipe(mi({breakOnErrors: false}));
 });
-
-gulp.task('js', function(cb) {
-	sequential('lint', 'mi', cb);
-});
-
-
-gulp.task('watch', function () {
-	gulp.watch(path, jsTasks);
-});
-
-/*!!
- *
- * Tareas por default
- *
- * tarea principal: gulp
- */
-
-gulp.task('default', function (cb) {
-	sequential('js', cb);
-});
-
--- source/2.js
+++
@@ -1,8 +0,0 @@
-function Cat(cat){
-	this.name = cat.name;
-	return this;
-}
-
-var kitty = new Cat({name: 'michi'});
-console.log(kitty.name);
-
